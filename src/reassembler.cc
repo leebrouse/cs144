614a84b17da1d5a70641899f0b0961507965aa80
leebrouse <leebrouse7@gmail.com>
2024-11-28 15:56:20 +0800

    pass the test 9 reassembler_single

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -19,23 +19,26 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
   // 1.If Reasssembler buffer is empty,push it(Intervals) into reBuffer
   // 2.check the available_capacity in byte_stream
   uint64_t available_capacity = output_.writer().available_capacity();
-  if ( reBuffer.empty() && first_index==0 ) {
+  if ( first_index == 0 ) {
     string subdata = data.substr( 0, min( data.length(), available_capacity ) );
+    // The process of reassemble
+    for ( const auto& interval : reBuffer ) {
+      // temporary method
+      if ( interval.data == data ) {
+        return;
+      }
+    }
     reBuffer.insert( { first_index, subdata, is_last_substring } );
     output_.writer().push( subdata );
-    return;
-  }
-
-  //The process of reassemble
-  for (const auto& interval:reBuffer)
-  {
-    if (interval.data==data)
-    {
-      break;
+  } else {
+    //Detect whether have condition of first_index == 0 in the reassemble buffer.
+    auto it = reBuffer.lower_bound( { 0, "", false } );
+    if ( it != reBuffer.end() && it->first_index == 0 ) {
+      string subdata = data.substr( 0, min( data.length(), available_capacity ) );
+      reBuffer.insert( { first_index, subdata, is_last_substring } );
+      output_.writer().push( subdata );
     }
-    
   }
-  
 
   /* If insert interval has is_last_substring tag.
     1.Reasssembler buffer clear.
@@ -43,10 +46,9 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
   */
 
   if ( is_last_substring ) {
-    output_.writer().close();
-    output_.reader().pop(reBuffer.size()-1);
+    output_.reader().pop( reBuffer.size() - 1 );
     reBuffer.clear();
-    return;
+    output_.writer().close();
   }
 }
 
